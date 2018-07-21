53855762a76081aee6d3f6148c563c6f09aad2af
Andre1900 <41217741+Andre1900@users.noreply.github.com>
2018-07-22 03:06:24 +0800

    Revert "Update index.php"

--- index.php
+++ index.php
@@ -6,8 +6,8 @@ Modified by Ilyasa
 */
 require_once('./line_class.php');
 
-$channelAccessToken = 'LFVOMa5G5veEsHi+QiMv5QKVvHHSixkmsacST6qyq6hYl6pV6IUKMLhtujp+/o/wfH39r/coSaZ6zn9m31ETzp/w0d4p1aETcrMKIeNnubCDA5LqAdBwiDQdLWdCfuZTSzeHM8Lt9A8v0bo1UnAWwgdB04t89/1O/w1cDnyilFU=';
-$channelSecret = '393941626768ff71f923286c9e78070e';
+$channelAccessToken = 'YOUR-CHANNEL-ACCESS-TOKEN'; //Your Channel Access Token
+$channelSecret = 'YOUR-CHANNEL-SECRET';//Your Channel Secret
 
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 
@@ -34,7 +34,7 @@ if($message['type']=='sticker')
 }
 else
 $pesan=str_replace(" ", "%20", $pesan_datang);
-$key = 'e4a620fe-3a6f-4d70-a6ce-5af26ec8afa7'; 
+$key = 'YOUR-API-KEY-SIMSIMI'; //API SimSimi
 $url = 'http://sandbox.api.simsimi.com/request.p?key='.$key.'&lc=id&ft=1.0&text='.$pesan;
 $json_data = file_get_contents($url);
 $url=json_decode($json_data,1);
